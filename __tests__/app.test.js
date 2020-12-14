61d2add9f2e23a68b031043536fa77501879fade
kevinfiero <kjf46@cornell.edu>
2020-12-14 15:14:22 -0800

    test for red route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,13 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('tests red GET route', async() =>{
+
+    const res = await request(app)
+      .get('/red');
+
+    expect(res.text).toEqual('<h1>red</h1>');
+
+  });
 
 });
